a2d8b0e17d6a9e5cba2e1a44b66edf36a2c0c881
Nikita <Coolguyplaylove@gmail.com>
2021-06-15 15:52:39 +0300

    maket

---
+++ maket/homepcc/js/Js.js
@@ -0,0 +1,11 @@
+const burger =document.querySelector(".catalog")
+const dropDownPointMessager =document.querySelector(".dropdownpointmessager")
+const allDialogsItems =document.querySelectorAll(".alldialogsitem")
+const messages =document.querySelectorAll(".message")
+
+
+burger.addEventListener("click", ()=>{
+
+    dropDownPointMessager.classList.toggle("active")
+    
+    })
